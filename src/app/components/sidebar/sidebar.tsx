9eba9f053e8e7bb0b0435a781aeaf481eaf3aeab
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-12-05 17:57:50 -0300

    initial commit

---
+++ src/app/components/sidebar/sidebar.tsx
@@ -0,0 +1,9 @@
+export function Sidebar() {
+    return (
+        <div className='pl-5 pt-5 mb-5 fixed'>
+            <div className="bg-slate-600 ">
+                Hello
+            </div>
+       </div>
+    )
+}
