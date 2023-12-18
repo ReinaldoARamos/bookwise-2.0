1e869f05a7ad7bfc5f620e48e33440f7ee392faa
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-12-18 17:43:21 -0300

    ajeitando a Tag Lida dos livros e também os paddings que estava deixando o texto desalinhado

--- src/app/components/ExplorerBooks/ExplorerBooksCard.tsx
+++ src/app/components/ExplorerBooks/ExplorerBooksCard.tsx
@@ -1,49 +1,54 @@
-'use client'
+"use client";
 import { Star } from "@phosphor-icons/react/dist/ssr";
-import React from 'react';
-import * as Dialog from '@radix-ui/react-dialog';
+import React from "react";
+import * as Dialog from "@radix-ui/react-dialog";
 import { ReviewDialog } from "../ReviewDialog/ReviewDialog";
 
-
 interface ExplorerBooksProps {
-    isRead?: boolean;
+  isRead?: boolean;
 }
 /* eslint-disable @next/next/no-img-element */
-export function ExplorerBooks({isRead} : ExplorerBooksProps) {
+export function ExplorerBooks({ isRead }: ExplorerBooksProps) {
   return (
-    <Dialog.Root >
-   <Dialog.Trigger>
-    <div className="bg-reviewCard rounded-lg flex ">
-        
-      <div className="px-5 py-4 flex  gap-5  ">
-      
-        <img src="/images/Book.png" width={108} height={152} alt="" />
-        <div className="flex flex-col justify-between">
-          <div className="flex flex-col">
-            <span className="text-md text-gray-100 font-bold "> Edude Edude</span>
-            <span className="text-sm text-gray-300">Edude Adventures</span>
+    <Dialog.Root>
+      <Dialog.Trigger>
+        <div className="flex flex-col rounded-lg bg-reviewCard">
+          <div className="flex justify-end">
+            {isRead ? (
+              <div className=" h-6 rounded-bl-lg rounded-tr-md bg-customgreen300  px-3 font-bold text-customgreen100 ">
+                LIDO
+              </div>
+            ) : (
+              <div className=" h-6 rounded-bl-lg rounded-tr-md bg-reviewCard  px-3 font-bold text-reviewCard "></div>
+            )}
           </div>
-          <div className="flex gap-[5px]">
-            <Star size={14} color="#8381D9" weight="fill"/> 
-            <Star size={14} color="#8381D9" weight="fill"/> 
-            <Star size={14} color="#8381D9" weight="fill"/> 
-            <Star size={14} color="#8381D9" /> 
-            <Star size={14} color="#8381D9"/> 
+          <div className="flex  gap-5   px-5 pb-6 ">
+            <img src="/images/Book.png" width={108} height={152} alt="" />
+            <div className="flex flex-col justify-between">
+              <div className="flex w-full flex-col items-start ">
+                <span className="text-md font-bold text-gray-100 ">
+                  {" "}
+                  Edude Edude
+                </span>
+                <span className="text-sm text-gray-300">Edude Adventures</span>
+              </div>
+              <div className="flex gap-[5px]">
+                <Star size={14} color="#8381D9" weight="fill" />
+                <Star size={14} color="#8381D9" weight="fill" />
+                <Star size={14} color="#8381D9" weight="fill" />
+                <Star size={14} color="#8381D9" />
+                <Star size={14} color="#8381D9" />
+              </div>
+            </div>
           </div>
         </div>
-       
-      </div>
-      <div>
-        {isRead ? <div className=" text-customgreen100 py-1 px-3 bg-customgreen300  font-bold rounded-bl-lg rounded-tr-md ">LIDO</div> : <div></div>}
-        </div>
-    </div>
-    </Dialog.Trigger>
+      </Dialog.Trigger>
 
-    <Dialog.Portal>
+      <Dialog.Portal>
         <Dialog.Overlay className="fixed inset-0 bg-black/50" />
       </Dialog.Portal>
-      <Dialog.Content className="fixed h-screen top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-4 rounded-md">
-        <div>oi</div>
+      <Dialog.Content className="fixed right-0 top-1/2 z-10 h-screen  -translate-y-1/2  transform rounded-md bg-white p-4">
+        <ReviewDialog />
       </Dialog.Content>
     </Dialog.Root>
   );
--- src/app/components/ReviewDialog/ReviewDialog.tsx
+++ src/app/components/ReviewDialog/ReviewDialog.tsx
@@ -1,5 +1,5 @@
 export function ReviewDialog() {
     return (
-        <div className="bg-black w-96 h-screen">Ola senhor</div>
+        <div className="bg-black  h-screen">Ola senhor</div>
     )
 }
