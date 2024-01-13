ea8e271dbb502604e8392beb9c676ef8141f4bb0
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2024-01-13 20:27:57 -0300

    criando um componente que com´primenta a pessoa com base no horário em que ela abre o site

---
+++ src/app/components/Greeting/Greeting.tsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+
+const GreetingComponent = () => {
+  const [greeting, setGreeting] = useState('');
+
+  useEffect(() => {
+    const getCurrentGreeting = () => {
+      const currentHour = new Date().getHours();
+
+      if (currentHour >= 6 && currentHour < 12) {
+        setGreeting('Bom dia, Início');
+      } else if (currentHour >= 12 && currentHour < 18) {
+        setGreeting('Boa tarde, Início');
+      } else {
+        setGreeting('Boa noite, Início');
+      }
+    };
+
+    getCurrentGreeting();
+
+    // Update greeting every minute
+    const interval = setInterval(getCurrentGreeting, 60000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+    
+      <p>{greeting}</p>
+    </div>
+  );
+};
+
+export default GreetingComponent;
