c800616adc5fb54ba0ddec2faa5405b69308f8bc
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2024-01-13 21:09:54 -0300

    criando o prisma adapter para criação de usuário ao logar, persistindo os dados

---
+++ src/app/@types/next-auth.d.ts
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth/next";
+
+declare module "next-auth" {
+    interface User {
+        id: string,
+        name: string,
+        avatar_url: string,
+        email: string ,
+
+    }
+
+    interface Session {
+        user: User
+    }
+}
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,11 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
 
+const prisma = new PrismaClient()
 export const authOption = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID ?? "",
@@ -11,9 +15,12 @@ export const authOption = {
 
   pages: {
     signIn: "/signin",
+    
   },
-};
 
+
+};
+//@ts-ignore
 const handler = NextAuth(authOption);
 
 export { handler as GET, handler as POST };
