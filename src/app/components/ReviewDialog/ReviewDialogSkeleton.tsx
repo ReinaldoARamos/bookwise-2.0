cbf87af4db18bd63f3b43f07c30e764010648ac8
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2024-01-15 13:54:33 -0300

    ajustando os tamanhos do skeleton do review dialog para nao vazer em telas de celular

--- src/app/components/ReviewDialog/ReviewDialogSkeleton.tsx
+++ src/app/components/ReviewDialog/ReviewDialogSkeleton.tsx
@@ -5,51 +5,55 @@ import Skeleton from "react-loading-skeleton";
 import { CommentsSkeleton } from "../Comments/CommentsSkeleton";
 
 export function ReviewDialogSkeleton() {
-  
-
-  
-
-
-
-  return  (
-   
-  
+  return (
     <>
       <div className="flex  flex-col rounded-[10px]  bg-reviewCard ">
         <div className="flex flex-col  px-8 py-6 ">
-          <div className="flex lg:gap-8 gap-4 border-b  border-b-gray-800 pb-10 ">
-            <Skeleton
-          width={172}
-          height={242}
-            
-            />
+          <div className="flex gap-4 border-b border-b-gray-800  pb-10 lg:gap-8 ">
+            <div className="block lg:hidden">
+              <Skeleton width={96} height={144} />
+            </div>
+            <div className="hidden lg:block">
+              <Skeleton width={172} height={242} />
+            </div>
             <div className="flex flex-col  justify-between">
-              <div className="flex flex-col space-y-2 ">
-                <Skeleton width={384} className=" max-w-[120px]   w-96 lg:max-w-sm   "/ >
-                
-            <Skeleton width={124} />
+            <div className="hidden lg:flex flex-col space-y-2 ">
+                <Skeleton width={384} />
+
+                <Skeleton width={124} />
+              </div>
+
+              <div className="flex flex-col space-y-2 lg:hidden">
+                <Skeleton width={124} />
+
+                <Skeleton width={72} />
               </div>
 
               <div className="flex flex-col space-y-1">
-              <Skeleton width={124} />
-             
-               
-                  <Skeleton width={124} />
+                <Skeleton width={124} />
+
+                <Skeleton width={124} />
               </div>
             </div>
           </div>
-          <div className="flex pt-6 gap-[60px]">
+          <div className="flex gap-[60px] pt-6">
             <div className="flex items-center gap-4">
               <BookmarkSimple size={24} className="text-singin" />
-              <div className="flex flex-col ">
+              <div className="hidden flex-col  lg:flex">
                 <Skeleton width={124} count={2} />
+              </div>
+              <div className="flex flex-col lg:hidden">
+                <Skeleton width={56} count={2} />
               </div>
             </div>
 
             <div className="flex items-center gap-4">
               <BookOpen size={24} className="text-singin" />
-              <div className="flex flex-col">
-             <Skeleton width={124} count={2} />
+              <div className="hidden flex-col  lg:flex">
+                <Skeleton width={124} count={2} />
+              </div>
+              <div className="flex flex-col lg:hidden">
+                <Skeleton width={56} count={2} />
               </div>
             </div>
           </div>
@@ -59,25 +63,15 @@ export function ReviewDialogSkeleton() {
       <div className="pt-[46px]">
         <div className="flex w-full justify-between pb-[22px]">
           <span className="text-sm text-gray-200">Avaliações</span>
-        
-                <span className="text-sm font-bold text-custompurple hover:cursor-pointer">
-                  Avaliar
-                </span>
-             
 
-           
+          <span className="text-sm font-bold text-custompurple hover:cursor-pointer">
+            Avaliar
+          </span>
         </div>
 
         <div className="flex flex-col space-y-3 rounded-lg">
-         
-            
-
-           <div className="flex flex-col space-y-3 rounded-lg" >
-        
-            <CommentsSkeleton
-           
-            />
-          
+          <div className="flex flex-col space-y-3 rounded-lg">
+            <CommentsSkeleton />
           </div>
         </div>
       </div>
