84562964c70d2678f1c45544641bff20a06aba19
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2024-01-16 11:27:04 -0300

    configurando o github provider

--- src/app/auth/authOptions.ts
+++ src/app/auth/authOptions.ts
@@ -1,55 +1,60 @@
 import PrismaAdapter from "@/lib/prisma-adapter";
 import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
-import GitHubProvider from "next-auth/providers/github"
+import GitHubProvider, { GithubProfile } from "next-auth/providers/github";
 import { redirect } from "next/navigation";
 
 export const authOption = {
-    adapter: PrismaAdapter(),
-    providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-        authorization: {
-          params: {
-            prompt: "consent",
-            access_type: "offline",
-            response_type: "code",
-            scope:
-              "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile ",
-          },
+  adapter: PrismaAdapter(),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+          scope:
+            "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile ",
         },
-        profile(profile: GoogleProfile) {
-          return {
-            id: profile.sub,
-            name: profile.name,
-            username: "",
-            email: profile.email,
-            avatar_url: profile.picture,
-            
-          };
-        },
-      }),
-      GitHubProvider({
-        clientId: process.env.GITHUB_ID ?? "",
-        clientSecret: process.env.GITHUB_SECRET ?? "",
-      })
-    ],
-  
-    callbacks: {
-      //@ts-ignore
-      async session({ session, user }) {
-      
+      },
+      profile(profile: GoogleProfile) {
         return {
-          ...session,
-          user,
+          id: profile.sub,
+          name: profile.name,
+          username: "",
+          email: profile.email,
+          avatar_url: profile.picture,
         };
+      },
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID ?? "",
+      clientSecret: process.env.GITHUB_SECRET ?? "",
+      //@ts-ignore
+      profile(profile: GithubProfile) {
+        return {
+          id: profile.id,
+          name: profile.name,
+          username: "",
+          email: profile.email,
+          avatar_url: profile.avatar_url,
+        };
+      },
+    }),
+  ],
 
-      
-      },
-         //@ts-ignore
-      async redirect() {
-        return "/";
-      },
+  callbacks: {
+    //@ts-ignore
+    async session({ session, user }) {
+      return {
+        ...session,
+        user,
+      };
     },
-  };
-  
+    //@ts-ignore
+    async redirect() {
+      return "/";
+    },
+  },
+};
